14f95fd6df492dce0e08f8e270cdd145473b3c9b
Rakehub <muabpab@gmail.com>
2026-06-21 17:31:27 +0200

    Fixed v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,17 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: 'no',
+    locales: ['no', 'en'],
+    routing: {
+      prefixDefaultLocale: false, // ESTO hace que el noruego sea la raíz (/)
+    },
+  },
   experimental: {
     viewTransitions: true,
   },
+
+  
   integrations: [tailwind(), compress()],
 });
