36203dbbc088c0a170fe36bac54da8fbf2a89b0a
Rakehub <muabpab@gmail.com>
2026-06-21 17:49:58 +0200

    eliminar i18

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,8 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-    defaultLocale: 'no',
-    locales: ['no', 'en'],
-    routing: {
-      prefixDefaultLocale: false, // ESTO hace que el noruego sea la raíz (/)
-    },
-  },
   experimental: {
     viewTransitions: true,
   },
-
-  
   integrations: [tailwind(), compress()],
 });
